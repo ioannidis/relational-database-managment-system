069ad3794e81d12945d952d1b51e7d9e06802b2e
Unknown <p16097@students.cs.unipi.gr>
2018-04-25 20:00:24 +0300

    2.1 and 2.3 MAX

--- 2.1.sql
+++ 2.1.sql
@@ -1,9 +1,20 @@
-SELECT public.car_models.title, COUNT(*) total_count 
+SELECT results.title,MAX(results.total_count) maxResults
 FROM(
+	SELECT car_models.title, COUNT(*) total_count 
+	FROM(
 		SELECT public.service_history.car_id AS cars
 		FROM public.service_history
 		INNER JOIN car_warehouse ON public.service_history.car_id=public.car_warehouse.id)y
-INNER JOIN car_models ON y.cars = public.car_models.id
+	INNER JOIN car_models ON y.cars = public.car_models.id
+	GROUP BY title
+	ORDER BY COUNT(*) DESC)results
+WHERE  results.total_count = (SELECT MAX(results.total_count) FROM(
+				SELECT car_models.title, COUNT(*) total_count 
+				FROM(
+				SELECT public.service_history.car_id AS cars
+				FROM public.service_history
+				INNER JOIN car_warehouse ON public.service_history.car_id=public.car_warehouse.id)y
+				INNER JOIN car_models ON y.cars = public.car_models.id
+				GROUP BY title
+				ORDER BY COUNT(*) DESC)results)
 GROUP BY title
-ORDER BY COUNT(*) DESC
-LIMIT 1
--- 2.3.sql
+++ 2.3.sql
@@ -1,5 +1,7 @@
-SELECT  buy_history.salesman_id,ROUND((sale_history.prices-buy_history.prices)::numeric,2) results
-   FROM 
+SELECT finalResults.employeeId,MAX(results) maximumProfit
+FROM(
+	SELECT  buy_history.salesman_id AS employeeId,ROUND((sale_history.prices-buy_history.prices)::numeric,2) results
+    FROM 
 			(SELECT salesman_id ,SUM(price) prices FROM sales_history
 			WHERE action='buy'
 			GROUP BY salesman_id
@@ -9,6 +11,20 @@ SELECT  buy_history.salesman_id,ROUND((sale_history.prices-buy_history.prices)::
 			WHERE action='sale'
 			GROUP BY salesman_id
 			ORDER BY prices DESC)sale_history
-   WHERE buy_history.salesman_id=sale_history.salesman_id
-   ORDER BY results DESC
-   LIMIT 1
+    WHERE buy_history.salesman_id=sale_history.salesman_id
+    ORDER BY results DESC)finalResults
+WHERE finalResults.results=(SELECT MAX(results) maximumProfit FROM(
+										SELECT  buy_history.salesman_id AS employeeId,ROUND((sale_history.prices-buy_history.prices)::numeric,2) results
+    									FROM 
+											(SELECT salesman_id ,SUM(price) prices FROM sales_history
+											WHERE action='buy'
+											GROUP BY salesman_id
+											ORDER BY prices DESC)buy_history,
+			
+											(SELECT salesman_id,SUM(price) prices FROM sales_history
+											WHERE action='sale'
+											GROUP BY salesman_id
+											ORDER BY prices DESC)sale_history
+    									WHERE buy_history.salesman_id=sale_history.salesman_id
+										ORDER BY results DESC)finalResults)
+GROUP BY finalResults.employeeId
