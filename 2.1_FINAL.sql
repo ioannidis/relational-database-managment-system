198272b32eefae37c54a3180faab1c13ab9e47d8
Panos Ioannidis <panos277@hotmail.com>
2018-04-28 15:31:37 +0300

    Create 2.1_FINAL.sql

---
+++ 2.1_FINAL.sql
@@ -0,0 +1,9 @@
+DROP VIEW IF EXISTS COUNT_MODELS;
+CREATE VIEW COUNT_MODELS AS (
+  SELECT car_models.id, car_models.title, count(car_models.id) AS num_of_model
+  FROM car_warehouse INNER JOIN car_models ON car_warehouse.model_id = car_models.id
+  GROUP BY car_models.id, car_models.title
+);
+
+SELECT * FROM COUNT_MODELS
+WHERE COUNT_MODELS.num_of_model = (SELECT max(num_of_model) AS maximum FROM COUNT_MODELS);
