049c305e597b7425b203e4454d8614aab0d221e5
Panos Ioannidis <panos277@hotmail.com>
2018-04-28 14:10:18 +0300

    Update 2.3.sql

--- 2.3.sql
+++ 2.3.sql
@@ -1,3 +1,36 @@
+DROP VIEW IF EXISTS SALES;
+CREATE VIEW SALES AS (
+  SELECT
+    A.salesman_id,
+    sum(A.price) AS sale
+  FROM sales_history AS A
+  WHERE A.action = 'sale'
+  GROUP BY A.salesman_id
+);
+
+DROP VIEW IF EXISTS BUYS;
+CREATE VIEW BUYS AS (
+  SELECT
+    A.salesman_id,
+    sum(A.price) AS buy
+  FROM sales_history AS A
+  WHERE A.action = 'buy'
+  GROUP BY A.salesman_id
+);
+
+DROP VIEW IF EXISTS PROFIT;
+CREATE VIEW PROFIT AS (
+  SELECT
+     SALES.salesman_id,
+     (SALES.sale - BUYS.buy) AS profit
+   FROM SALES NATURAL JOIN BUYS
+);
+
+SELECT salesman_id, employee.first_name, employee.last_name, profit
+FROM PROFIT INNER JOIN employee ON PROFIT.salesman_id = employee.id
+WHERE profit = (SELECT max(profit) FROM PROFIT);
+
+-- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
 SELECT finalResults.employeeId,MAX(results) maximumProfit
 FROM(
 	SELECT  buy_history.salesman_id AS employeeId,ROUND((sale_history.prices-buy_history.prices)::numeric,2) results
