cef71d15c8f046b2c3a409f2797b4755ef4f4ef2
Panos Ioannidis <panos277@hotmail.com>
2018-04-28 14:34:19 +0300

    Update 2.5.sql

--- 2.5.sql
+++ 2.5.sql
@@ -1,9 +1,9 @@
 -- Prepei na mas dieukrinisoun an orizoun os ergasia tin enarksi i tin liksi, an einai i to 1 i to allo to ienai to parakato
+-- Leei gia ton X, opote emeis ipothetoume oti leei gia ton mastora me id 3
 SELECT first_name,last_name,car_id,start_date,end_date
-FROM employee
-INNER JOIN service_history ON employee.id=service_history.tech_id
-WHERE end_date > NOW() - INTERVAL '1 month'
-OR (end_date IS NULL AND start_date > NOW() - INTERVAL '1 month');
+FROM employee INNER JOIN service_history ON employee.id=service_history.tech_id
+WHERE employee.id = 3 AND (end_date > NOW() - INTERVAL '1 month'
+OR (end_date IS NULL AND start_date > NOW() - INTERVAL '1 month'))
 
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
