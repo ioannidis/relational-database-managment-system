6149693c185dde117961c31c2ca7275a91e38657
Panos Ioannidis <panos277@hotmail.com>
2018-05-06 13:34:38 +0300

    Update and rename 2.5.sql to 2.5_FINAL.sql

--- 2.5_FINAL.sql
+++ 2.5_FINAL.sql
@@ -4,12 +4,3 @@ SELECT first_name,last_name,car_id,start_date,end_date
 FROM employee INNER JOIN service_history ON employee.id=service_history.tech_id
 WHERE employee.id = 3 AND (end_date > NOW() - INTERVAL '1 month'
 OR (end_date IS NULL AND start_date > NOW() - INTERVAL '1 month'))
-
-
--- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-SELECT first_name,last_name,car_id,start_date,end_date
-FROM employee
-RIGHT JOIN service_history ON employee.id=service_history.tech_id
-WHERE end_date > NOW() - INTERVAL '30 days'
-OR	start_date > NOW() - INTERVAL '30 days'
-OR end_date IS NULL
