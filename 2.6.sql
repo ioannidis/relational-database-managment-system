ca691ee827fdf8be10348c81fb45102ef9c56f7f
Panos Ioannidis <panos277@hotmail.com>
2018-05-13 16:47:33 +0300

    Update and rename 2.6.sql to 2.6_FINAL.sql

--- 2.6.sql
+++
@@ -1,8 +0,0 @@
-SELECT title,car_warehouse.id,COUNT(*) total_count FROM service_history
-INNER JOIN car_warehouse ON service_history.car_id=car_warehouse.id
-INNER JOIN car_models ON car_warehouse.model_id=car_models.id
-WHERE (end_date <= NOW() - INTERVAL '1 year'
-OR	start_date <= NOW() - INTERVAL '1 year')
-GROUP BY title,car_warehouse.id
-HAVING COUNT(*) > 1
-ORDER BY total_count DESC
